5784203c32a34b9a858c414ed49ccb23b7d44ab9
leungtinyau27m5 <leungtinyau27m5@gmail.com>
2019-04-15 23:55:19 +0800

    add notebook path

--- src/Server/config.js
+++ src/Server/config.js
@@ -1,8 +1,11 @@
 export const serverConn = {
+    serverUri: 'http://192.168.0.114:80/shopAroundYou/shopAroundYou/Auth.php',
+    serverGoogleMapUri: 'http://192.168.0.114:80/shopAroundYou/shopAroundYou/Google_Map/googleMap.php',
+    serverAssets: 'http://192.168.0.114:80/shopAroundYou/shopAroundYou/assets/',
     //serverUri: 'https://steveleungtest321.000webhostapp.com/',
     //serverGoogleMapUri: 'https://steveleungtest321.000webhostapp.com/Google_Map/googleMap.php',
     //serverAssets: 'https://steveleungtest321.000webhostapp.com/assets/'
-    serverUri: 'http://192.168.0.103:80/shopAroundYou/Auth.php',
-    serverGoogleMapUri: 'http://192.168.0.103/shopAroundYou/Google_Map/googleMap.php',
-    serverAssets: 'http://192.168.0.103:80/shopAroundYou/assets/',
+    //serverUri: 'http://192.168.0.103:80/shopAroundYou/Auth.php',
+    //serverGoogleMapUri: 'http://192.168.0.103/shopAroundYou/Google_Map/googleMap.php',
+    //serverAssets: 'http://192.168.0.103:80/shopAroundYou/assets/',
 }
